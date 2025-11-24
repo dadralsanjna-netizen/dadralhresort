bbf570bb8e6ad5b76b007cf6aea0bb850b38bed0
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-11-24 15:19:47 +0530

    setting

--- project1/settings.py
+++ project1/settings.py
@@ -284,12 +284,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Additional locations of static files
 STATICFILES_DIRS = [
-    BASE_DIR / "app1" / "static",
+    os.path.join(BASE_DIR, 'static'),
 ]
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
