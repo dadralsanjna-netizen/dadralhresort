abe8d40628a9ce90e8b19407374762aca15e5875
Tushukk31059 <138555958+Tushukk31059@users.noreply.github.com>
2025-11-24 16:22:54 +0530

    chnge

--- project1/settings.py
+++ project1/settings.py
@@ -196,12 +196,14 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'whitenoise.runserver_nostatic',
     'django.contrib.staticfiles',
     'app1',
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
